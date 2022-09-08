fb807f38a8163180a82555ee69f25e08253c2f2d
Henry <kanuchigozie7@gmail.com>
2022-09-08 22:53:56 +0000

    Task 0 - Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
