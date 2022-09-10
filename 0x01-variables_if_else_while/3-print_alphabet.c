0718f9518b20d6cdc56823e9383d6d33c79c418b
Henry <kanuchigozie7@gmail.com>
2022-09-10 00:16:16 +0000

    Task 3 - uppercase and lowercase alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the alphabet in lowercase
+ * and then in uppercase
+ *
+ * Return: Always 0 (succes)
+ */
+
+int main(void)
+{
+	int i = 97;
+
+	while i < 123;
+	{
+		putchar(i)
+		i++;
+	}
+	putchar(10);
+
+	return (0);
+}
