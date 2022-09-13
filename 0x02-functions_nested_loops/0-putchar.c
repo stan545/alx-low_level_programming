4711156eab993f3de87066916bbca1dc7a5a0b25
Henry <kanuchigozie7@gmail.com>
2022-09-13 19:44:10 +0000

    Task 0-Print _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,14 +11,16 @@
 
 int main(void)
 {
-	char text[10] = "_putchar";
-	int i = 0;
-
-	for (i = 0; i < 9; i++)
 	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
-
+	}
 	return (0);
 }
