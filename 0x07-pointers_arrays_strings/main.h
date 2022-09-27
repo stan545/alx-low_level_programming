1a48d274e21873d09aeaa91799f867cbb38e67dc
Henry <kanuchigozie7@gmail.com>
2022-09-27 23:40:48 +0000

    set string

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+
+void set_string(char **s, char *to);
 int _putchar(char c);
 void simple_print_buffer(char *buffer, unsigned int size);
 char *_memset(char *s,char b,unsigned int n);
