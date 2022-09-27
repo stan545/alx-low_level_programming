61cbf4aaae075dd386248a7b4c31578a55fd1845
Henry <kanuchigozie7@gmail.com>
2022-09-27 22:38:04 +0000

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,13 +10,13 @@ char *_strchr(char *s, char c)
 {
 	char *s1 = s - 1;
 
-	do{
+	do {
 		s++;
 		if (*s1 == c)
 		{
 			return (s1);
 		}
-	}while (*s1 != '\0');
+	} while (*s1 != '\0');
 
 	return (0);
 }
