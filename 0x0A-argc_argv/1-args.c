6b7f95f21e6c12a6e5ab060f92baca8f0a33a780
stan545 <kanuchigozie7@gmail.com>
2022-09-30 14:27:09 -0400

    program that prints number of argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main- Entry point that arguments
+* @argv: Argument vector it holds the command line arguments
+* @argc: Argument count which counts the number of arguments given.
+* Return: 0 if successful.
+*/
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc);
+	return (0);
+}
+
