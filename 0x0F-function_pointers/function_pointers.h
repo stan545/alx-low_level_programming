c86f9f146a1ab8e2eca5c4df03f2c126a2161c9b
Henry <kanuchigozie7@gmail.com>
2022-10-13 01:36:02 +0000

    fixed header file

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,14 +1,11 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-/* This is all the main protypes*/
+#include <stdlib.h>
+
+/* Thes are all the main protypes*/
 void print_name(char *name, void (*f)(char *));
 int int_index(int *array, int size, int (*cmp)(int));
-
-/**
-  * _putchar - writes a char c to stdout
-  * @c: character t be printed
-  * Return: 1 on success, otherwise -1 (error)
-  */
+void array_iterator(int *array, size_t size, void (*action)(int));
 int _putchar(int c);
 #endif
