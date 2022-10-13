1e95409dbf41195c4960b5158ef392fd4970a80d
Henry <kanuchigozie7@gmail.com>
2022-10-13 01:29:52 +0000

    fixed function, to check

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -6,10 +6,10 @@
  * @name: a string passed as argument to the function
  * @f: a funtion pointer
  */
-
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
-	(*f)(name);
+
+	f(name);
 }
