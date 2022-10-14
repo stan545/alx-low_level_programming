51da9031253ea77f30f7b2feb41f4ada4b89da3d
Henry <kanuchigozie7@gmail.com>
2022-10-14 00:31:56 +0000

    fix e0err,

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,6 +14,13 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_list ptr;
 
+	if (n == 0)
+	{
+		printf("\n");
+		return;
+	}
+
+
 	va_start(ptr, n);
 	for (i = 0; i < n; i++)
 	{
