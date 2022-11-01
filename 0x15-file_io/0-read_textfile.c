8879ae4b62cd934029dcbc350b4f22f02ab21c3d
Henry <kanuchigozie7@gmail.com>
2022-11-01 00:07:27 +0000

    Resolved error

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,23 +15,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int fd;
 	char *text;
 
-	if (!filename)
+	if (filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	text = malloc(sizeof(char) * letters + 1);
 	if (text == NULL)
 		return (0);
-	nlettersRead = read(fd, text, sizeof(char) * letters);
+
+	nlettersRead = read(fd, text, letters);
 	if (nlettersRead == -1)
 		return (0);
+
 	text[letters + 1] = '\0';
 	close(fd);
 
 	nlettersWritten = write(STDOUT_FILENO, text, nlettersRead);
 	if (nlettersWritten == -1)
 		return (0);
+
 	free(text);
 
 	return (nlettersRead);
