3f93e7804ce4f099f8b5678edcbe9cf0cbcab08d
stan545 <kanuchigozie7@gmail.com>
2023-02-28 16:11:01 +0000

    Added a little correction in comments

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 
 /**
  * _abs - a function that computes the absolute value of an integer
- * @n: an ingeger variable
+ * @n: an integer variable
  * Return: the positive value
  */
 int _abs(int n)
