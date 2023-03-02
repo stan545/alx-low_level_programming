a61fdf278d25652733c5048fa8f5c764a8c82a6c
stan545 <kanuchigozie7@gmail.com>
2023-03-02 20:24:45 +0000

    Added fix to bug

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,3 +1,4 @@
+carrie@ubuntu:/debugging$ cat 2-largest_number.c
 #include "main.h"
 
 /**
@@ -12,11 +13,11 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a >= b && b <= c)
+	if (a >= b && b >= c)
 	{
 		largest = a;
 	}
-	else if (b >= a && a <= c)
+	else if (b >= a && a >= c)
 	{
 		largest = b;
 	}
